217b454052de51a82cfc55a2b7298b7ffb31123f
Peter Lohmer <p.lohmer@googlemail.com>
2012-12-19 14:34:38 +0100

    extension
    added new dsp commands , version incremented
    
    Signed-off-by: Peter Lohmer <p.lohmer@googlemail.com>

--- zdspglobal.h
+++ zdspglobal.h
@@ -20,12 +20,21 @@
 //v1.05 ergänzt :
 //	neuer befehl für dsp (SQRT) eingeführt .... läuft aber nur mit dsp ab V3.06
 //v1.06 fehler bereinigt @ gencmd für cmdi16if32 fehler bei 1. parameter wurde nicht gemeldet
+//v1.07 ergänzt :
+//  neue befehle für dsp
+//  {"COPYVAL",61,CMD2i16,0},
+//  {"CMPCLK",62,CMD3i16,0},
+//  {"CLKMODE",63,CMD1i16,0},
+//  {"GETSTIME",64,CMD1i16,0},
+//  {"TESTTIMESKIPLT",65,CMD2i16,0}
+//  eingeführt. ..... läuft aber erst mit dsp ab version 3.07
+
 
 //#define CheckSumOffset 56
 //#define LeiterkartenName "wm3000i"
 #define DSPDeviceNode "/dev/zFPGA1dsp1"
 #define ServerBasisName "zdsp1d"
-#define ServerVersion "V1.06"
+#define ServerVersion "V1.07"
 #define InpBufSize 4096
 
 // wenn DEBUG -> kein fork() 
