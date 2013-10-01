d8ca25aa70689548ea98f004d0687c9a5d4ae389
Peter Lohmer <peterlohmer01@gmail.com>
2013-10-01 08:32:22 +0200

    zdspglobal.h: version number changed
    Signed-off-by: Peter Lohmer <peterlohmer01@gmail.com>

--- zdspglobal.h
+++ zdspglobal.h
@@ -33,12 +33,18 @@
 // neue befehle für dsp
 // {"SUBVCC",66,CMD3i16,0}}
 //  eingeführt. ..... läuft aber erst mit dsp ab version 3.08
-
+//v1.09:
+//  ressourcemanager support added, linux event loop changed to qt event loop(qcoreapllication)
+//  dsp var decoding uses nhash tables now (qhash)
+//  server configuration with xml implemented
+//  new dsp command {"DSPINTPOST,69,CMD1i16,0}
+//  serialized dsp interrupts support implemented
+//  meas. modules merging DSPINTPOST added at end of list
 
 
 #define DSPDeviceNode "/dev/zFPGA1dsp1"
 #define ServerBasisName "zdsp1d"
-#define ServerVersion "V1.08"
+#define ServerVersion "V1.09"
 #define InpBufSize 4096
 #define defaultXSDFile "/etc/zera/zdsp1d/zdsp1d.xsd"
 
