2ab096118535f0f46d58def2d333da400caeb6c0
Peter Lohmer <peterlohmer01@gmail.com>
2015-10-28 21:44:35 +0100

    dsp.cpp: added command DSPINTPOST for new interrupthandling
    Signed-off-by: Peter Lohmer <peterlohmer01@gmail.com>

--- dsp.cpp
+++ dsp.cpp
@@ -132,7 +132,7 @@
 
 #include "dsp.h"
 
-static sDspCmd DspCmd[68] = {	{"INVALID", 0, CMD ,0},
+static sDspCmd DspCmd[69] = {	{"INVALID", 0, CMD ,0},
 				{"USERMEMOFFSET", 1, CMD1i32, 0 },
 				{"DSPMEMOFFSET", 2, CMD1i32, 0 },
 				{"COPYDATA", 3, CMD3i16, 0 },	
@@ -199,7 +199,8 @@ static sDspCmd DspCmd[68] = {	{"INVALID", 0, CMD ,0},
                 {"GETSTIME",64,CMD1i16,0},
                 {"TESTTIMESKIPNEX",65,CMD2i16,0},
                 {"SUBVCC",66,CMD3i16,0},
-                {"SUBVVG",67,CMD3i16,0}};
+                {"SUBVVG",67,CMD3i16,0},
+                {"DSPINTPOST", 68, CMD, 0}};
 
 
 sDspCmd* findDspCmd(QString& s)
